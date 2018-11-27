11603ef306f8ea1540f229b9eba843556d79e5de
Li Liao <lee.liaoli@gmail.com>
2018-11-27 17:47:38 -0600

    cpp tutorial

--- main.cpp
+++ main.cpp
@@ -19,9 +19,37 @@ using namespace std;
 /*
  * 
  */
+
+string concatenate(string& a, string& b){
+    
+    a = "change a";
+    b = "change b";
+    return a+b;
+}
+
+template <class T>
+T sum(T a, T b){
+    T result;
+    result = a + b;
+    return result;
+}
+
 int main(int argc, char** argv) {
     
     cout<<"hello world."<<endl;
+    
+    string a = "aaaaa";
+    string b = "bbbbb";
+    
+    cout<<concatenate(a, b)<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    
+    int i = 5, j = 6;
+    double id = 5.0, jd = 6.0;
+    
+    cout<<sum<int>(i, j)<<endl;
+    cout<<sum<double>(id, jd)<<endl;
 
     return 0;
 }
